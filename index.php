f0a580723cd2656221d7c2599e85e6784691118c
Thud <thuddesigns@gmail.com>
2023-04-04 04:18:28 -0700

    node.js version

--- index.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Redirect all requests to the proxy script
-header('Location: proxy.php?url=' . urlencode($_GET['url']));
-?>
--- proxy.php
+++
@@ -1,6 +0,0 @@
-<?php
-$url = $_GET['url'];
-$response = file_get_contents($url);
-header('Access-Control-Allow-Origin: *');
-echo $response;
-?>
